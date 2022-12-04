347382a3de96264acd03ddad1a9775630b8d2888
maxime <pheline.maxime@yahoo.fr>
2022-12-04 21:50:44 +0100

    day4 v3

--- day4p2.js
+++ day4p2.js
@@ -4,9 +4,7 @@ FS.readFile('inputs/inputday4', 'utf-8', (err, data) => {
     data.split('\n').forEach(line => {
         let sections = line.split(/[-,]+/) 
         if ((Number(sections[0]) >= Number(sections[2]) && Number(sections[0]) <= Number(sections[3])) ||
-            (Number(sections[1]) >= Number(sections[2]) && Number(sections[1]) <= Number(sections[3])) ||
-            (Number(sections[2]) >= Number(sections[0]) && Number(sections[2]) <= Number(sections[1])) ||
-            (Number(sections[3]) >= Number(sections[0]) && Number(sections[3]) <= Number(sections[1]))) {
+            (Number(sections[2]) >= Number(sections[0]) && Number(sections[2]) <= Number(sections[1]))) {
             pairs ++ 
         }
     })
