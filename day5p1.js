da9808da2c7293087838f2323f4f56d208cc63df
maxime <pheline.maxime@yahoo.fr>
2022-12-05 23:41:10 +0100

    day 5

---
+++ day5p1.js
@@ -0,0 +1,45 @@
+const FS = require('fs')
+let stacks = {
+    1:[],
+    2:[],
+    3:[],
+    4:[],
+    5:[],
+    6:[],
+    7:[],
+    8:[],
+    9:[]
+}
+
+
+FS.readFile('inputs/inputday5.txt', 'utf-8', (err, data) => {
+    let parts = data.split('\n\n')
+    parts[0].split('\n').forEach(line => {
+        line.split('').forEach((char, i) => {
+            if (isNaN(char)) {
+                switch (i) {
+                    case 1 : stacks[1].push(char) ; break ;
+                    case 5 : stacks[2].push(char) ; break ;
+                    case 9 : stacks[3].push(char) ; break ;
+                    case 13 : stacks[4].push(char) ; break ;
+                    case 17 : stacks[5].push(char) ; break ;
+                    case 21 : stacks[6].push(char) ; break ;
+                    case 25 : stacks[7].push(char) ; break ;
+                    case 29 : stacks[8].push(char) ; break ;
+                    case 33 : stacks[9].push(char) ; break ;
+                    default : break ;
+                }
+            }
+        })
+    })
+
+    parts[1].split('\n').forEach(line => { 
+        let values = line.split(' ')
+        for (let i = 1 ; i <= values[1] ; i++) {
+            stacks[values[5]].unshift(stacks[values[3]][0])
+            stacks[values[3]].shift()
+        }
+        
+    })
+    console.log(stacks)
+})
