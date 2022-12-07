4c17667cd0512903ea860efc21e5b7c1b22e3564
maxime <pheline.maxime@yahoo.fr>
2022-12-07 07:50:31 +0100

    day6 v2

--- day6p1.js
+++ day6p1.js
@@ -1,24 +1,18 @@
+const { table } = require('console')
 const FS = require('fs') 
+
 const formattedInput = FS.readFileSync('inputs/inputday6', { encoding: 'utf-8'}).trim()
-
-let resultat = day6(formattedInput)
-console.log(resultat)
-
-function day6(input) {
-    let resultat = null ; 
-    input.trim().split('').forEach((char, index) => {
-        let traitement = input.substring(index, index+4)
-        let present = false 
-        traitement.split('').forEach((char) => {
-            let compa = traitement.replace(char, '') 
-            if (compa.includes(char)){
-                present = true
-            }
-        })
-        if (!present) {
-            resultat = (index+4)
-            return resultat
+for (let index = 0; index < formattedInput.split('').length; index++) {
+    let traitement = formattedInput.substring(index, index+4)
+    let present = false 
+    traitement.split('').forEach((char) => {
+        let compa = traitement.replace(char, '') 
+        if (compa.includes(char)){
+            present = true
         }
     })
-    return resultat
+    if (!present) {
+        console.log('Résultat => ' + (index+4))
+        break
+    }
 }
