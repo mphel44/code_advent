25fd15377c40f609cc71a2462f2f8b37a1a5e39f
Maxime PHELINE <101396015+mphel44@users.noreply.github.com>
2022-12-08 17:25:27 +0100

    day8 v2

--- day8.js
+++ day8.js
@@ -12,6 +12,8 @@ data.replace(/(\r)/gm,'').split('\n').forEach((line, y) => {
     })
 })
 console.log(forest)
+console.log("LARGEUR => " + forest[0].length)
+console.log("HAUTEUR => " + forest.length)
 
 
 let exteriorVisibleTrees = ((forest.length * 2)-4) + (forest[0].length*2)
@@ -19,42 +21,53 @@ let exteriorVisibleTrees = ((forest.length * 2)-4) + (forest[0].length*2)
 let interiorVisibleTrees = []
 data.replace(/(\r)/gm,'').split('\n').forEach((line, y) => {
     line.split('').forEach((tree, x) => {
-        let visible = true 
+ 
+
         //CHECK SUR LA DROITE 
-        if (x != 0 && x != forest[0].length-1) {
-            for (let i = x+1; i<forest[0].length-x-1; i++) {
+        if (x>0 && x<(forest[0].length-1) && y>0 && y<(forest.length-1)) {
+
+            let visible = true 
+            console.log('nouvelle ligne')
+
+            for (let i = x; i<forest[0].length-x; i++) {
                 if (tree <= forest[y][i]) {
+                    //console.log('Arbre DROITE [' + y + '][' + x + '] plus petit que' + forest[y][i])
                     visible = false ;
                 }
             }
     
             //CHECK SUR LA GAUCHE 
-            for (let i = x-1; i>=1 ; i--) {
-                if (tree < forest[y][i]) {
+            for (let i = x; i>=0 ; i--) {
+                if (tree <= forest[y][i]) {
+                    //console.log(tree)
+                    //console.log('Arbre GAUCHE [' + y + '][' + x + ']')
                     visible = false ;
                 }
             }
-        }
-
-        if (y != 0 && y != forest.length-1) {
+        
 
             //CHECK VERS LE BAS 
-            for (let i = y+1; i<forest.length-y-1; i++) {
-                if (tree < forest[i][x]) {
+            for (let i = y; i<forest.length-y; i++) {
+                if (tree <= forest[i][x]) {
+                    //console.log('Arbre BAS [' + y + '][' + x + ']')
                     visible = false ;
                 }
             }
             //CHECK SUR LE HAUT 
-            for (let i = y-1; i>=1 ; i--) {
-                if (tree < forest[i][x]) {
+            for (let i = y; i>=0 ; i--) {
+                if (tree <= forest[i][x]) {
+                    //console.log('Arbre HAUT [' + y + '][' + x + ']')
                     visible = false ;
                 }
             }
+
+            if (visible) {
+                console.log('arbre[' + y + '][' + x + '] est visible de lexterieur')
+                interiorVisibleTrees.push(tree)
+            }
         }
 
-        if (visible) {
-            interiorVisibleTrees.push(tree)
-        }
+
     })
 })
 
