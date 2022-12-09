ef78d67492a2288668d8bf8d26a6ce95fb19a697
maxime <pheline.maxime@yahoo.fr>
2022-12-09 07:23:46 +0100

    day9 v2

--- day9.js
+++ day9.js
@@ -2,4 +2,39 @@ const FS = require('fs')
 const data = FS.readFileSync('inputs/inputday9.txt', { encoding: 'utf-8'}).trim()
 
 
+let gride = []
+let position = []
+
+//initial pos
+let x = 1 
+let y = 1
+gride.push([x,y])
+
+data.replace(/(\r)/gm,'').split('\n').forEach(line => {
+    let directionData = line.split(' ')
+    switch (directionData[0]) {
+        case 'R' :
+        case 'L' :
+        case 'D' :
+        default :
+    }
+
+        if (isNaN(char)) {
+
+
+
+
+            console.log(char + ' est la DIRECTION à prendre')
+        } else {
+            console.log(char + ' est la distance à parcourir ! ')
+        }
+
+
+})
+
+
+
+
+//result
+console.log('Rope visited ' + gride.length + ' positions !')
 
